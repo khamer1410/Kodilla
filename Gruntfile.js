85fb121455c73cddf0bdb81b281ef41d892d453c
Hamer <kkhmer@gmail.com>
2017-01-07 19:47:10 +0100

    Prepare to merge with grunt addition

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function(grunt) {
                 expand: true,
                 cwd: 'images/',
                 src: ['**/*.{png,jpg,gif}'],
-                dest: 'images/build/'
+                dest: 'images/'
                     }]
                   }
               },
